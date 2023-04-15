9908b884529f015a68bdde7992bbe7a19cc71c9e
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-04-15 07:40:29 -0700

    Delivers a really cool message.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const replaceText = () => {
+    const text = document.querySelector("#text");
+    text.textContent = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", replaceText);
